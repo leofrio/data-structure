2393ca00d73fcb53e501394e1898b95c439af7fa
Leonardo Pontes Gaspar <49174917+leofrio@users.noreply.github.com>
2019-10-29 19:11:33 -0700

    Update Main.java

--- Java/BlockCryptography/Main.java
+++ Java/BlockCryptography/Main.java
@@ -1,52 +1,49 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-    	Crypto c=new Crypto();
-    	Scanner let=new Scanner(System.in); 
-    	String key="";
-    	String word="";
-    	int blocksize=-4343344;
-    	System.out.println("esse e um programa que ira criptografar baseado me chaes de palavras digitadas pelo o usuario");
-    	for(int i=0;true;i++) { 
-    		if(i ==0) {
-    			System.out.println("por favor digite a chave"); 
-    			key=let.nextLine(); 
-    			System.out.println("por favor digite o tamanho do bloco");
-    			blocksize=let.nextInt();
-    		}
-    		System.out.println("escolha uma das opcoes abaixo"); 
-    		System.out.println("1-criptografar uma palavra");
-    		System.out.println("2-descriptografar uma palavra");
-    		System.out.println("3-alterar a chave");
-    		System.out.println("4-alterar o tamanho do bloco");
-    		System.out.println("5-sair");
-    		int choice=let.nextInt();
-    		if(choice == 1) {
-    			let.nextLine();
-    			System.out.println("digite a palavra a ser criptografada");
-    			word=let.nextLine(); 
-    			c.crypto(word, key, blocksize);
-    		} 
-    		if(choice == 2) {
-    			let.nextLine();
-    			System.out.println("digite a palavra a ser decriptografada");
-    			word=let.nextLine(); 
-    			c.Decrypto(word, key, blocksize);
-    		} 
-    		if(choice == 3) {
-    			let.nextLine();
-    			System.out.println("digite a nova chave");
-    			key=let.nextLine();
-    		} 
-    		if(choice == 4) {
-    			System.out.println("digite o novo tamanho do bloco");
-    			blocksize=let.nextInt();
-    		}
-    		if(choice == 5) {
-    			break;
-    		}
-    	}
-    	System.out.println("the end");
+        public static void main(String[] args) {
+            Crypto c = new Crypto();
+            Scanner let = new Scanner(System.in);
+            String key = "";
+            String word = "";
+            int blocksize =- 1;
+            for (int i = 0; true; i++) {
+                if(i == 0) {
+                    System.out.println("Digite a chave para criptografar ou descriptografar: ");
+                    key=let.nextLine();
+                    System.out.println("Digite o tamanho do bloco: ");
+                    blocksize=let.nextInt();
+                }
+
+                System.out.println("Selecione uma operação: " +
+                        "\n- 1) Criptografar uma frase" +
+                        "\n- 2) Descriptografar uma frase" +
+                        "\n- 3) Alterar a chave" +
+                        "\n- 4) Alterar o tamanho do bloco" +
+                        "\n- 5) Finalizar programa");
+                int choice = let.nextInt();
+                if(choice == 1) {
+                    let.nextLine();
+                    System.out.println("Digite a frase que será criptografada: ");
+                    word = let.nextLine();
+                    c.crypto(word, key, blocksize);
+                } else if(choice == 2) {
+                    let.nextLine();
+                    System.out.println("Digite a frase que será descriptografada: ");
+                    word = let.nextLine();
+                    c.Decrypto(word, key, blocksize);
+                } else if(choice == 3) {
+                    let.nextLine();
+                    System.out.println("Digite a nova chave: ");
+                    key = let.nextLine();
+                } else if(choice == 4) {
+                    System.out.println("Digite o novo tamanho do bloco: ");
+                    blocksize = let.nextInt();
+                } else if(choice == 5) {
+                    break;
+                } else {
+                    System.out.println("Operação Inválida!");
+                }
+            }
+        }
     }
-}
